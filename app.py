16a5cb6ea89ae87086861ffea4766261e86ec876
marc <marcharvey1470@gmail.com>
2020-03-31 23:10:30 -0500

    finished very basic login concept

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-#app.py
-# This will likely be converted into just the login portion
+# app.py
+# This will likely be converted into just the login portion backend
 
 from flask import Flask, request, render_template, jsonify
 from flask_mysqldb import MySQL
@@ -9,38 +9,20 @@ from string import ascii_letters, digits
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'  # IP of db host
-app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_USER'] = 'testuser'
 app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'user_info'
+app.config['MYSQL_DB'] = 'userinfo'
 
 mysql = MySQL(app)
 
 
-# This is recieved before a login or create_user
-@app.route('/get_salt', methods=['POST'])
-def get_salt():
-    # Get username
-    username = request.form.get('username')
-
-    # Cursor
-    cur = mysql.connection.cursor()
-    print(mysql.connection)
-
-    # Get salt
-    cur.execute("SELECT salt FROM users WHERE username = '%s';" % username)
-    if cur.rowcount < 1:
-        salt = ''.join(choice(ascii_letters + digits) for i in range(20))
-    elif cur.rowcount == 1:
-        salt = cur.fetchone()[0]
-
-    # Craft response
-    response = jsonify({'salt': salt})
-    
-    return response
-
-
-# Need to make sure only app/website or whatever can access this api
-@app.route('/create_user', methods=['POST'])
+# create_user:
+# 
+# Frontend "create user" connects directly to this.
+# The frontend is responsible for creating salt.
+# TODO: This should require email verification.
+#
+@app.route('/create-user', methods=['POST'])
 def create_user():
     # Get user inputs
     email = request.form.get('email')
@@ -48,39 +30,82 @@ def create_user():
     passhash = request.form.get('passhash')  # Bcrypt
     salt = request.form.get('salt')
 
-    # Get automatic inputs (location, device, etc)
-    # TODO, needs interaction from frontends
+    # TODO: Get automatic inputs (location, device, etc) needs interaction from frontends
 
     # Cursor
     cur = mysql.connection.cursor()
 
+    # TODO: Make sure email is a valid email
+
     # Validate inputs, just check they don't already exist. Password will be checked by frontends
     cur.execute("SELECT username FROM users WHERE username = '%s' OR email = '%s';" % (username, email))
-    if cur.rowcount < 1:
+    if cur.rowcount >= 1:
         return "Username or email already exists!"  # This will eventually just return some structure which says this. 
 
     # Add user to database
-    cur.execute("INSERT INTO users (username, email, passhash, salt) VALUES ('%s', '%s', '%s', '%s');" % (username, email, passhash, salt))
+    cur.execute("INSERT INTO users (username, email, passhash, salt, verified) VALUES ('%s', '%s', '%s', '%s', false);" % (username, email, passhash, salt))
+    mysql.connection.commit()
 
     # Craft response
     response = jsonify({'token': 'dummy-token'})
     return "<h1>Welcome, %s </h1>" % username  # This will return a success, as well as an authentication token
 
 
-@app.route('/validate_user', methods=['POST'])
+# get_salt
+#
+# This is recieved after login button on frontend,
+# but before the validation.
+# If this fails, we will return a "user doesn't exist error"
+#
+@app.route('/get-salt', methods=['POST'])
+def get_salt():
+    # Get username
+    username = request.form.get('username')
+    email = request.form.get('username')
+
+    # Cursor
+    cur = mysql.connection.cursor()
+
+    # Get salt
+    cur.execute("SELECT salt FROM users WHERE username = '%s' OR email = '%s';" % (username, email))
+    if cur.rowcount < 1:
+       return "No such user"
+    salt = cur.fetchall()[0]
+
+    # Craft response
+    response = jsonify({'salt': salt})
+    
+    return response
+
+
+# validate_user
+#
+# This is the actual user validation funtion during
+# login. This assumes the salt has been gotten by frontend
+# already.
+#
+@app.route('/validate-user', methods=['POST'])
 def validate_user():
     # Get user inputs
+    username = request.form.get('username')
+    email = request.form.get('username')
+    passhash = request.form.get('passhash')
 
-    # Get automatic inputs (location, device, etc)
+    # TODO: Get automatic inputs (location, device, etc)
 
     # Cursor
     cur = mysql.connection.cursor()
 
     # Validate inputs, check username/email exists, check passhash matches
+    cur.execute("SELECT userid, username FROM users WHERE (username = '%s' OR email = '%s') AND passhash = '%s';" % (username, email, passhash))
+    if cur.rowcount < 1:
+        return "Bad username or password"
 
-    # Create token
+    # Create auth token
+    token = "testtoken"
 
     # Craft response
+    return token
 
 
 # This is for testing the api. Realistically, you should use Postman
@@ -88,17 +113,28 @@ def validate_user():
 def form():
 
     return '''
-            <p>Test create user</p>
-            <form method="POST" action="/create_user">
+            <h2>Test create user</h2>
+            <p>This is to create a new user</p>
+            <form method="POST" action="/create-user">
                 email: <input type="text" name="email"><br>
                 username: <input type="text" name="username"><br>
                 passhash: <input type="text" name="passhash"><br>
+                salt: <input type="text" name="salt"><br>
                 <input type="submit" value="Submit"><br>
-            </form>
+            </form><br>
 
-            <p>Test get salt</p>
-            <form method="POST" action="/get_salt">
-                username: <input type="text" name="salt"><br>
+            <h2>Test get salt</h2>
+            <p>This will be used after user presses login, but before validation</p>
+            <form method="POST" action="/get-salt">
+                username: <input type="text" name="username"><br>
                 <input type="submit" value="Submit"><br>
-            </form>
+            </form><br>
+
+            <h2>Test validate user</h2>
+            <p>This will be called after the frontend has the salt. The salt and password</p>
+            <form method="POST" action="/validate-user">
+                username/email: <input type="text" name="username"><br>
+                passhash: <input type="text" name="passhash"><br>
+                <input type="submit" value="Submit"><br>
+            </form><br>
             '''
