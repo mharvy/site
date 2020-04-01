34317081d2720f9feae29abad3a2460669ea4f70
marc <marcharvey1470@gmail.com>
2020-04-01 01:44:41 -0500

    added a post database

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'  # IP of db host
 app.config['MYSQL_DATABASE_USER'] = 'testuser'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'userinfo'
+app.config['MYSQL_DATABASE_DB'] = 'maindb'
 
 app.register_blueprint(auth_app)
 
--- setup_database.sh
+++ setup_database.sh
@@ -2,4 +2,6 @@
 
 exec mysql < setup_database/db_setup.txt &
 sleep 1
-exec mysql -u testuser -p < setup_database/userinfo_setup.txt
+exec mysql -u testuser -p < setup_database/users_setup.txt &
+sleep 10
+exec mysql -u testuser -p < setup_database/posts_setup.txt
