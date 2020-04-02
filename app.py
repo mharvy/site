7eaf79c4af859704cb1d80d589e79a9850320c64
marc <marcharvey1470@gmail.com>
2020-04-02 18:26:49 -0500

    changed directory name from modules to api

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from extensions import mysql
-from modules.auth.auth import auth_app
-from modules.post.post import post_app
-from modules.comment.comment import comment_app
+from api.auth.auth import auth_app
+from api.post.post import post_app
+from api.comment.comment import comment_app
 
 
 app = Flask(__name__)
